68e1b2dbf6ee357a6d99088aa8cf858459b7d916
Stephan Dilly <dilly.stephan@gmail.com>
2019-08-05 16:45:49 +0200

    small fixes

--- Makefile
+++ Makefile
@@ -1,5 +1,4 @@
 
 run:
-	cd docs/
-	mkdir dst || true
+	cd docs && mkdir dst || true
 	docker run -it -v $(shell pwd)/docs:/ocr --entrypoint "/bin/bash" jbarlow83/ocrmypdf -c 'cd /ocr && ./processInbox.sh'
--- docs/processInbox.sh
+++ docs/processInbox.sh
@@ -5,5 +5,5 @@ for f in $FILES
 do
   echo "Processing '$f'"
   # take action on each file. $f store current file name
-  ocrmypdf -l eng+deu "$f" "../dst/$f"
+  /appenv/bin/ocrmypdf -l eng+deu "$f" "../dst/$f"
 done
